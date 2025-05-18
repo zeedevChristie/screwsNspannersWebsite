24bb0967d695dd34f3ed986d3f7efe65ed5e2fa6
unknown <christiecares01@gmail.com>
2025-05-18 20:43:17 +0100

    done

--- src/pages/FAQpage/FaqPage.jsx
+++ src/pages/FAQpage/FaqPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../../components/navBar/nav";
-import Footer from "../../components/footer/Footer";
+import Footer from "../../components/footer/footer";
 import FAQ from "../../components/accordion/Faq";
 
 const FaqPage = () => {
