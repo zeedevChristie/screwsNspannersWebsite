d2d3f39d61f175e0fb8e640de07a64d488efea05
unknown <christiecares01@gmail.com>
2025-05-18 20:40:05 +0100

    corrections made

--- src/pages/homePage/home.jsx
+++ src/pages/homePage/home.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Navbar from "../../components/navBar/nav";
 import Statistics from "../../components/landingSection/statistics";
-// import Footer from "../../components/footer/footer";
+import Footer from "../../components/footer/footer";
 import WelcomeCarousel from "../../components/welcome/welcomeCarousel";
 import HowToSection from "../../components/howToSection/howToSection";
-import Footer from "../../components/footer/footer";
 
 const Home = () => {
   return (
