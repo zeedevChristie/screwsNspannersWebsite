25869cc3f1b7230d308f18698216aba6dce30b22
unknown <christiecares01@gmail.com>
2025-05-18 20:59:13 +0100

    rechecked

--- src/pages/Services/ServicePage.jsx
+++ src/pages/Services/ServicePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/navBar/nav'
-import Footer from '../../components/footer/Footer'
+import Footer from '../../components/footer/footer'
 import ServiceDelivery from './ServiceDelivery'
 
 const ServicePage = () => {
