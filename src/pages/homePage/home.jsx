a6d2a4174ad6bfc4a8ba8aa29bfa3e3a02a0b476
unknown <christiecares01@gmail.com>
2025-05-18 20:35:27 +0100

    responsivenessPush

--- src/pages/homePage/home.jsx
+++ src/pages/homePage/home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "../../components/navBar/nav";
 import Statistics from "../../components/landingSection/statistics";
-import Footer from "../../components/footer/footer";
+// import Footer from "../../components/footer/footer";
 import WelcomeCarousel from "../../components/welcome/welcomeCarousel";
 import HowToSection from "../../components/howToSection/howToSection";
+import Footer from "../../components/footer/footer";
 
 const Home = () => {
   return (
@@ -16,7 +17,7 @@ const Home = () => {
         <WelcomeCarousel />
         <Statistics />
         <HowToSection />
-      <Footer />                                
+      <Footer/>                    
       </main>
 
     </div>
